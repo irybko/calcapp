7f10312445a5b7a10c8e542bffe928992b95a5b9
irybko <irybko@gmail.com>
2022-09-09 03:36:17 +0300

    Create checking.pl

---
+++ src/v1/checking.pl
@@ -0,0 +1,43 @@
+%
+%	checking.pl
+%       Es - Expression Codes
+%
+:- consult('lib/checklists.pl').
+
+%
+%	checktype rule checks argument`s data type which
+%	would be an integer, float or list of integer or float.
+%       Argument`s type can not be a string
+%
+
+checktype(X) :- 
+	integer(X); 
+	float(X).
+
+checktype(X) :- 
+	string(X), 
+	write("Argument has incompatible type"), 
+	halt.
+
+checktype([H|T]) :-
+	checktype(H),
+	checktype(T).
+
+checkop(Expr, OpCode) :-
+	string_codes(Expr, Es),
+	(
+		char_code('*', OpCode),	
+		member(OpCode, Es) -> 
+		!;
+		char_code('/', OpCode),	
+		member(OpCode, Es) -> 
+		!;
+		char_code('+', OpCode),	
+		member(OpCode, Es) -> 
+		!;
+		char_code('-', OpCode),	
+		member(OpCode, Es) ->
+		!;
+		write("Operator does not found")
+	),!.
+
