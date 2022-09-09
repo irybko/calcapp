45c469e38340e3ad01510c91002d2e41f3d7b6a7
irybko <irybko@gmail.com>
2022-09-09 03:38:18 +0300

    Create loadlib.pl

---
+++ src/v1/loadlib.pl
@@ -0,0 +1,10 @@
+%
+%	consult all files
+%
+:- consult('lib/checklists.pl').
+:- consult('lib/listpreds.pl').
+:- consult('lib/servpreds.pl').
+:- consult('lib/sortpreds.pl').
+:- consult('lib/streampreds.pl').
+:- consult('lib/strpreds.pl').
+:- consult('lib/strtonum.pl').
